d0897ab93bbe77d7aad8391c9a04976501e5f619
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-31 09:11:27 -0300

    Create loop-sets.py

---
+++ loop-sets.py
@@ -0,0 +1,9 @@
+# LOOPS MEDIANTE SETS
+# Python nos permite crear bucles a través de un set con el keyword for
+
+print('LOOPS THROUGH SET') # Muestro título.
+colors = {'red', 'green', 'blue'} # Creo var set y sus items.
+for items in colors: # Creo var 'item' para que tome el valor de cada elemento en la var 'colors'
+  print(items) # Muestro var 'item'. Será 'red' 'blue' green'.
+  
+  
